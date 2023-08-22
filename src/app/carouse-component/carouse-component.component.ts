6f4bf947684d0dbae3cba328cbb9fe88165ec5be
Nilton Clasen <nduducbnu@gmail.com>
2023-08-21 21:44:25 -0300

    carousel

---
+++ src/app/carouse-component/carouse-component.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-carouse-component',
+  templateUrl: './carouse-component.component.html',
+  styleUrls: ['./carouse-component.component.css']
+})
+export class CarouseComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+    step = 0;
+    selectedValue: string = "";
+
+    foods: Food[] = [
+      {value: 'steak-0', viewValue: 'Steak'},
+      {value: 'pizza-1', viewValue: 'Pizza'},
+      {value: 'tacos-2', viewValue: 'Tacos'},
+    ];
+
+    setStep(index: number) {
+      this.step = index;
+    }
+
+    nextStep() {
+      this.step++;
+    }
+
+    prevStep() {
+      this.step--;
+    }
+
+}
